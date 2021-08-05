5bdce31e88ae80f25c51c00bdaaa9bc3c7a34b6b
Eduardo-LP <eduardolemospedroso@gmail.com>
2021-08-04 22:01:50 -0300

    1.0.0/adiciona arquivos iniciais com imgs em carrossel

---
+++ src/main/java/code/com/desafio/appEduardoLemos/AppEduardoLemosApplication.java
@@ -0,0 +1,13 @@
+package code.com.desafio.appEduardoLemos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppEduardoLemosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppEduardoLemosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/code/com/desafio/appEduardoLemos/AppEduardoLemosApplicationTests.java
@@ -0,0 +1,13 @@
+package code.com.desafio.appEduardoLemos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppEduardoLemosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
